1f03ee56cc03f809a1c6f91a9ecf5842f2d6110a
Marcelo Domhoff <marcelo.domhoff@patelhs.org>
2021-02-22 13:04:08 +0000

    First method, may change later

--- Main.java
+++ Main.java
@@ -1,8 +1,16 @@
 class Main {
   public static void main(String[] args) {
-    
+    counting(5);
   }
 
   //test
-  public static void 
+  public static void counting(int a){
+    if(a == 0){
+      System.out.println("Done, I can't remember anything else!");
+    }else{
+      System.out.println(a);
+      a--;
+      counting(a);
+    }
+  }
 }
