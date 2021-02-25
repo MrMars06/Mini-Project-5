f1335ba612e92b5d93a02121604792e37a0a34ad
Marcelo Domhoff <marcelo.domhoff@patelhs.org>
2021-02-25 17:20:48 +0000

    Got rid of second test method

--- Main.java
+++ Main.java
@@ -4,13 +4,7 @@ class Main {
 
     Scanner scan = new Scanner(System.in);
 
-    System.out.println("What do you want your first value to be for recursion #2? to be?");
-    int valOne = scan.nextInt();
-    System.out.println("What do you want your second value for recursion #2 to be?");
-    int valTwo = scan.nextInt();
-
     counting(10);
-    addition(5);
   }
 
   //test
@@ -21,17 +15,6 @@ class Main {
       System.out.println(a);
       a--;
       counting(a);
-    }
-  }
-
-  //another test
-  public static void addition(int b){
-    if(b == 0){
-      System.out.println("Done!");
-    }else{
-      System.out.println(valOne + valTwo);
-      b--;
-      addition(b);
     }
   }
 }
