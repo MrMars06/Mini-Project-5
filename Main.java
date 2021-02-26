f900cd55e8e0119209978741aa9533b79c919d74
Marcelo Domhoff <marcelo.domhoff@patelhs.org>
2021-02-26 13:26:44 +0000

    Added some comments throughout code

--- Main.java
+++ Main.java
@@ -2,8 +2,10 @@ import java.util.Scanner;
 class Main {
   public static void main(String[] args) {
 
+    //creates scanner
     Scanner scan = new Scanner(System.in);
 
+    //prints both recursions
     counting(10);
     acceptValidinput(scan);
     System.out.println("Your input is valid");
@@ -20,6 +22,7 @@ class Main {
     }
   }
 
+  //accepValidinput recursion w/ scanner
   public static void acceptValidinput(Scanner scan){
     System.out.println("Print a number that is greater than 10");
     int i = scan.nextInt();
